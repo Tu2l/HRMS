9de0f09eb2801844124be2fcf3576c247ad40491
Tutul Deb Roy <debroytutul@gmail.com>
2022-05-25 16:05:42 +0530

    db connection updated

--- Backend/connection.js
+++ Backend/connection.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";    
 
-mongoose.connect("mongodb://127.0.0.1:27017/gratia_hrms")
+const CON = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/gratia_hrms"
+mongoose.connect(CON, { useNewUrlParser: true, useUnifiedTopology: true })
 
 export const connection = mongoose.connection
